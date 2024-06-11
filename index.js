5b371a2dcc4964be89695aa4e8de7b5a95fc270a
evertonnlimma <140436663+evertonnlimma@users.noreply.github.com>
2024-06-11 14:02:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function show() {
+  const card = document.querySelector(".card");
+  card.style.display = "none";
+
+  const flowers = document.querySelector(".flowers");
+  flowers.style.display = "flex";
+}
